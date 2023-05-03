d7ac54ae9cf572364786ac7ed904f3fbd529ef06
James Gaither <jmgaither@outlook.com>
2023-05-03 16:04:56 +0900

    Modify dotfiles to use nix home-manager

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Home Manager configuration of jgaither";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dotfiles = {
+      url = "github:jamesgaither/dotfiles";
+      flake = false;
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, dotfiles, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."jgaither" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+        extraSpecialArgs = {
+          inherit dotfiles;
+        };
+      };
+    };
+}
