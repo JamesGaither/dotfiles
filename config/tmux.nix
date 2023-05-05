9f8e51e2ba6edb2367362a41a8acab76b8974179
James Gaither <jmgaither@outlook.com>
2023-05-05 10:40:38 +0900

    Attempt to use git submodules

--- config/tmux.nix
+++ config/tmux.nix
@@ -5,6 +5,16 @@
     tmux = {
       enable = true;
       mouse = true;
+      prefix = "C-a";
+      plugins = with pkgs; [
+        tmuxPlugins.resurrect
+      ];
+      extraConfig = ''
+        bind -r k select-pane -U
+        bind -r j select-pane -D
+        bind -r h select-pane -L
+        bind -r l select-pane -R
+      '';
     };
   };
 }
--- home.nix
+++ home.nix
@@ -6,7 +6,7 @@
   ];
   home = {
     username = "jgaither";
-    homeDirectory = "/home/$jgaither";
+    homeDirectory = "/home/jgaither";
     stateVersion = "22.11";
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -40,19 +40,7 @@
     # '')
     ];
   };
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
     ".zshrc".source = config/.zshrc;
     ".p10k.zsh".source = config/.p10k.zsh;
     #".tmux.conf".source = config/.tmux.conf;
