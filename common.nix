a3aed947af96fc6f44a5276d440637adb871392c
James Gaither <jmgaither@outlook.com>
2023-05-15 00:06:49 +0900

    First attempt to add multiple system setups in flakes

---
+++ common.nix
@@ -0,0 +1,62 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./config/tmux.nix
+    ./config/zsh.nix
+    ./config/nvim/neovim.nix
+  ];
+  home = {
+    username = "jgaither";
+    homeDirectory = "/home/jgaither";
+    stateVersion = "22.11";
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    packages = with pkgs; [
+      gcc
+      vim
+      tmux
+      git
+      meslo-lgs-nf
+      ripgrep
+
+    ];
+    shellAliases = {
+      g = "git";
+    };
+  };
+  home.file = {
+    ".p10k.zsh".source = config/.p10k.zsh;
+    ".vimrc".source = config/.vimrc;
+  };
+  xdg.configFile = {
+    nvim = {
+      source = config/nvim/lua;
+      recursive = true;
+    };
+    pypoetry.source = config/.config/pypoetry;
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/jgaither/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  fonts.fontconfig.enable = true;
+}
--- flake.nix
+++ flake.nix
@@ -19,18 +19,14 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations.jgaither = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-        extraSpecialArgs = {
-          #inherit dotfiles;
+      homeConfigurations = {
+       "desktop" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ 
+            ./desktop.nix
+          ];
         };
+
       };
     };
 }
