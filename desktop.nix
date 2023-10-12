047d0660b54ac22ab82762e70dd570ee817ec5e8
James Gaither <jmgaither@outlook.com>
2023-10-12 21:57:46 +0900

    Modify desktop so gui based programs show up in ubuntu launcher

--- desktop.nix
+++ desktop.nix
@@ -10,4 +10,10 @@
       sqlitebrowser
     ];
   };
+  # As already mentioned
+  targets.genericLinux.enable = true;
+  xdg.mime.enable = true;
+
+  # The critical missing piece for me
+  xdg.systemDirs.data = [ "${config.home.homeDirectory}/.nix-profile/share/applications" ];
 }
