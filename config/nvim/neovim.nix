8b193786026310f6b89c33cb272973c76597e0c0
James Gaither <jmgaither@outlook.com>
2023-10-26 22:27:31 +0900

    Add coc rust-anaylzer and python

--- config/nvim/neovim.nix
+++ config/nvim/neovim.nix
@@ -5,6 +5,7 @@
     enable = true;
     viAlias = true;
     vimAlias = true;
+    coc.enable = true;
     extraPackages = with pkgs; [
       tree-sitter
     ];
@@ -16,6 +17,8 @@
       onenord-nvim
       gitsigns-nvim
       undotree
+      coc-rust-analyzer
+      coc-python
     ];
     extraConfig = ''
       :luafile .config/nvim/lua/init.lua
