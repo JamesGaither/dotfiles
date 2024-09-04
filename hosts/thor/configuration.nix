06b6e465c0dbe6c28632012228af523a9c6a15b2
James Gaither <jmgaither@outlook.com>
2024-09-04 21:40:55 +0900

    Add system76 hardware support to thor

--- hosts/thor/configuration.nix
+++ hosts/thor/configuration.nix
@@ -54,6 +54,9 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  
+  hardware.system76.enableAll = true;
+  services.power-profiles-daemon.enable = false;
   # Enable cosmic-desktop alpha
   #services.desktopManager.cosmic.enable = true;
   #services.displayManager.cosmic-greeter.enable = true;
