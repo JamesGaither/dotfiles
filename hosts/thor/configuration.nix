5c7290b2d2b61e7f5ef00d1cf3e88bde0f4abfc5
James Gaither <jamesgaither@proton.me>
2024-10-06 21:55:31 +0900

    Add ansible to thor sysfiles

--- hosts/thor/configuration.nix
+++ hosts/thor/configuration.nix
@@ -105,6 +105,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    ansible
     vim
     obsidian
     gnomeExtensions.pop-shell
