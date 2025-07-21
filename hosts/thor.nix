905636960dd8e1b4319d970f872d9871625e8d58
James Gaither <github@jamesgaither.com>
2025-07-21 21:04:03 +0900

    Change journal entry to include date header

--- hosts/thor.nix
+++ hosts/thor.nix
@@ -1,8 +1,10 @@
 { config, pkgs, ...}:
 {
+  home.sessionVariables = {
+    LEDGER_FILE = "~/Documents/share_docs/Financial/ledger/2025.journal";
+  };
   programs.zsh.shellAliases = {
-    # Double backslash is needed as nix escapes 1
-    journal = "nvim ~/Documents/personal_vault/01\\ journal/$(date +%Y%m%d).md";
+    journal = ''[ -f ~/journal/Daily/$(date +%Y%m%d).md ] || echo "# Journal for $(date '+%A, %B %-d')$(date '+%d' | sed 's/\b\(1[123]\|[^1]1\)\b/th/;s/\b[^1]2\b/nd/;s/\b[^1]3\b/rd/;t;s/.*/th/') $(date +%Y)" > ~/journal/Daily/$(date +%Y%m%d).md; nvim ~/journal/Daily/$(date +%Y%m%d).md'';
     hm = "home-manager switch --flake ~/.dotfiles#thor";
   };
 }
