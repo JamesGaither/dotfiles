15feb90bf94da9bc9c2b19bba09ee9050d69cfe8
James Gaither <github@jamesgaither.com>
2026-01-05 09:27:36 +0900

    Add correct ledger entry to hosts

--- hosts/oden.nix
+++ hosts/oden.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ...}:
 {
   home.sessionVariables = {
-    LEDGER_FILE = "~/Documents/share_docs/Financial/ledger/all.journal";
+    LEDGER_FILE = "~/Documents/share_docs/Financial/ledger/2026.journal";
   };
   programs.zsh.shellAliases = {
     journal = ''[ -f ~/journal/Daily/$(date +%Y%m%d).md ] || echo "# Journal for $(date '+%A, %B %-d')$(date '+%d' | sed 's/\b\(1[123]\|[^1]1\)\b/th/;s/\b[^1]2\b/nd/;s/\b[^1]3\b/rd/;t;s/.*/th/') $(date +%Y)" > ~/journal/Daily/$(date +%Y%m%d).md; nvim ~/journal/Daily/$(date +%Y%m%d).md'';
